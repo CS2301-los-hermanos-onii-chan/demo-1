44bf76b6a18ba8a6282bbd558ab5e676f84adfa7
otreblan <otreblain@gmail.com>
2021-11-21 18:02:26 -0500

    Read and magic

--- demo-1.cpp
+++ demo-1.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <vector>
+
+int magic()
+{
+	int m = 0;
+
+	for(size_t i = 0; i < sizeof(m); i+=2)
+	{
+		m |= (1 << i);
+	}
+
+	return m;
+}
+
+int crc_remainder()
+{
+	int divisor = magic();
+
+	return 0;
+}
+
+std::vector<bool> read()
+{
+	int c;
+	std::vector<bool> v;
+
+	while((c = getchar()) != -1)
+	{
+		for(size_t i = 0; i < 8; i++)
+		{
+			v.push_back(c & (1 << (7 - i)));
+		}
+	}
+
+	return v;
+}
 
 int main()
 {
+
 	std::cout << "Hello world\n";
 }
